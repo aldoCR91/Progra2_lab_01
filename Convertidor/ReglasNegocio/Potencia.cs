8438139b217f7ac8202088f3c99cc428331de0af
Aldo Burgos <56781359+aldoCR91@users.noreply.github.com>
2023-09-18 19:37:46 -0600

    Update Potencia.cs
    Comentarios en la clase Potencia

--- Convertidor/ReglasNegocio/Potencia.cs
+++ Convertidor/ReglasNegocio/Potencia.cs
@@ -8,8 +8,10 @@ namespace Convertidor.ReglasNegocio
 {
     internal class Potencia
     {
+        // clase estatica para no tener que instanciar la clase
         public static string Calcular(int base1, int potencia) 
         {
+            // retorna un string con el resultado del metodo pow de la clase math
             return Math.Pow(base1, potencia).ToString();
         }
     }
